81d398eabe2e9ddb9f63af9e63b4a2d739e07e82
Rorywork <Rorywork95@outlook.com>
2022-02-09 10:35:12 +0000

    YH Changes to Engineers

--- quotepad/views/yh_personnel.py
+++ quotepad/views/yh_personnel.py
@@ -1,39 +1,30 @@
 SURVEYOR_DROPDOWN = (
 	('','Select One'),
 	('tom.hewitt@yourheat.co.uk', 'Tom Hewitt'),
-	('jeremy.tomkinson@yourheat.co.uk', 'Jeremy Tomkinson'),
-	('lee.hewitt@yourheat.co.uk', 'Lee Hewitt'),
-	('tom.driscoll@yourheat.co.uk', 'Tom Driscoll'),	
+	('tom.driscoll@yourheat.co.uk', 'Tom Driscoll'),
+	('carn.prior@yourheat.co.uk', 'Carn Prior')	
 )
 
 ENGINEER_DROPDOWN = (
 	('','Select One'),
 	('kevin.harvey@yourheat.co.uk', 'Kevin Harvey'),
 	('pikeben28@yahoo.co.uk', 'Ben Pike'),
-	('petersheatingservices@yahoo.com', 'Matthew Peters'),
 	('dave_easton@me.com', 'Dave Easton'),
-	('adouglas241@googlemail.com', 'Andy Douglas'),
-	('adam.simmons@yourheat.co.uk', 'Adam Simmons'),
-	('terry.martin@yourheat.co.uk', 'Terry Martin'),
 	('tom.sharp@yourheat.co.uk', 'Tom Sharp'),
-	('keith.myler-dixon@yourheat.co.uk', 'Keith Myler-Dixon'),
-	('maydayheating@hotmail.com', 'Martin May'),
-	('alfiefitz1987@outlook.com', 'Alfie Fitzpatrick')
+	('alfiefitz1987@outlook.com', 'Alfie Fitzpatrick'),
+	('liam.barnes@yourheat.co.uk', 'Liam Barnes'),
+	('george.downs@yourheat.co.uk', 'George Downs')
 )
 
 ENGINEER_POSTCODE_DROPDOWN = (
 	('','Select One'),
 	('Kevin Harvey (SM5)', 'Kevin Harvey (SM5)'),
 	('Ben Pike (SS12)', 'Ben Pike (SS12)'),
-	('Matthew Peters (ME8)', 'Matthew Peters (ME8)'),
 	('Dave Easton (ME14)', 'Dave Easton (ME14)'),
-	('Andy Douglas (ME12)', 'Andy Douglas (ME12)'),
-	('Adam Simmons (TN10)', 'Adam Simmons (TN10)'),
-	('Terry Martin (TN17)', 'Terry Martin (TN17)'),
 	('Tom Sharp (EN9)', 'Tom Sharp (EN9)'),
-	('Keith Myler-Dixon (CM2)', 'Keith Myler-Dixon (CM2)'),
-	('Martin May (KT)', 'Martin May (KT)'),
 	('Alfie Fitzpatrick (TN25)', 'Alfie Fitzpatrick (TN25)'),
+	('Liam Barnes (ME12)', 'Liam Barnes (ME12)'),
+	('George Downs (SS7)', 'George Downs (SS7)'),
 	('Stock Availability Check', 'Stock Availability Check')
 )
 
@@ -55,7 +46,9 @@ engineer_dict = {
 	'tom.sharp@yourheat.co.uk': 'Tom Sharp',
 	'keith.myler-dixon@yourheat.co.uk': 'Keith Myler-Dixon',
 	'maydayheating@hotmail.com': 'Martin May',
-	'alfiefitz1987@outlook.com': 'Alfie Fitzpatrick'
+	'alfiefitz1987@outlook.com': 'Alfie Fitzpatrick',
+	'liam.barnes@yourheat.co.uk': 'Liam Barnes',
+	'george.downs@yourheat.co.uk': 'George Downs'
 }
 
 engineer_postcode_dict = {
@@ -69,7 +62,9 @@ engineer_postcode_dict = {
 	'Tom Sharp (EN9)': 'tom.sharp@yourheat.co.uk',
 	'Keith Myler-Dixon (CM2)': 'keith.myler-dixon@yourheat.co.uk',
 	'Martin May (KT)': 'maydayheating@hotmail.com',
-	'Alfie Fitzpatrick (TN25)': 'alfiefitz1987@outlook.com'
+	'Alfie Fitzpatrick (TN25)': 'alfiefitz1987@outlook.com',
+	'Liam Barnes (ME12)' : 'liam.barnes@yourheat.co.uk',
+	'George Downs (SS7)' : 'george.downs@yourheat.co.uk'
 }
 
 engineer_calendar_dict = {
@@ -83,5 +78,7 @@ engineer_calendar_dict = {
 	'Tom Sharp': 'tom.sharp@yourheat.co.uk',
 	'Keith Myler-Dixon': 'keith.myler-dixon@yourheat.co.uk',
 	'Martin May': 'martin.may@yourheat.co.uk',
-	'Alfie Fitzpatrick': 'alfie.fitzpatrick@yourheat.co.uk' 
+	'Alfie Fitzpatrick': 'alfie.fitzpatrick@yourheat.co.uk',
+	'Liam Barnes': 'liam.barnes@yourheat.co.uk',
+	'George Downs': 'george.downs@yourheat.co.uk' 
 }
