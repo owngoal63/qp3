02e696b236f7f57761eb6191f83ec6a16ec28c35
Rorywork <Rorywork95@outlook.com>
2022-11-21 21:06:13 +0000

    Multiple changes to personnel

--- quotepad/views/yh_personnel.py
+++ quotepad/views/yh_personnel.py
@@ -1,89 +1,43 @@
 SURVEYOR_DROPDOWN = (
 	('','Select One'),
 	('tom.hewitt@yourheat.co.uk', 'Tom Hewitt'),
-	('tom.driscoll@yourheat.co.uk', 'Tom Driscoll'),
-	('carn.prior@yourheat.co.uk', 'Carn Prior')	
+	('alan.brooks@yourheat.co.uk', 'Alan Brooks')	
 )
 
 ENGINEER_DROPDOWN = (
 	('','Select One'),
-	('kevin.harvey@yourheat.co.uk', 'Kevin Harvey'),
-	('pikeben28@yahoo.co.uk', 'Ben Pike'),
-	('dave_easton@me.com', 'Dave Easton'),
-	('tom.sharp@yourheat.co.uk', 'Tom Sharp'),
 	('alfiefitz1987@outlook.com', 'Alfie Fitzpatrick'),
-	('liam.barnes@yourheat.co.uk', 'Liam Barnes'),
-	('george.downs@yourheat.co.uk', 'George Downs'),
-	('carn.prior@yourheat.co.uk', 'Carn Prior')
+	('alan.brooks@yourheat.co.uk', 'Alan Brooks'),
+	('nick.cook@yourheat.co.uk', 'Nick Cook')
 )
 
 ENGINEER_POSTCODE_DROPDOWN = (
 	('','Select One'),
-	('Kevin Harvey (SM5)', 'Kevin Harvey (SM5)'),
-	('Ben Pike (SS12)', 'Ben Pike (SS12)'),
-	('Dave Easton (ME14)', 'Dave Easton (ME14)'),
-	('Tom Sharp (EN9)', 'Tom Sharp (EN9)'),
 	('Alfie Fitzpatrick (TN25)', 'Alfie Fitzpatrick (TN25)'),
-	('Liam Barnes (ME12)', 'Liam Barnes (ME12)'),
-	('George Downs (SS7)', 'George Downs (SS7)'),
-	('Carn Prior (TN24)', 'Carn Prior (TN24)'),
+	('Alan Brooks (ME6)', 'Alan Brooks (ME6)'),
+	('Nick Cook (ME6)', 'Nick Cook (ME6)'),
 	('Stock Availability Check', 'Stock Availability Check')
 )
 
 surveyor_dict = {
 	'tom.hewitt@yourheat.co.uk': 'Tom Hewitt',
-	'jeremy.tomkinson@yourheat.co.uk': 'Jeremy Tomkinson',
-	'lee.hewitt@yourheat.co.uk': 'Lee Hewitt',
-	'tom.driscoll@yourheat.co.uk': 'Tom Driscoll'	
+	'alan.brooks@yourheat.co.uk': 'Alan Brooks'	
 }
 
 engineer_dict = {
-	'kevin.harvey@yourheat.co.uk': 'Kevin Harvey',
-	'pikeben28@yahoo.co.uk': 'Ben Pike',
-	'petersheatingservices@yahoo.com': 'Matthew Peters',
-	'dave_easton@me.com': 'Dave Easton',
-	'adouglas241@googlemail.com': 'Andy Douglas',
-	'adam.simmons@yourheat.co.uk': 'Adam Simmons',
-	'terry.martin@yourheat.co.uk': 'Terry Martin',
-	'tom.sharp@yourheat.co.uk': 'Tom Sharp',
-	'keith.myler-dixon@yourheat.co.uk': 'Keith Myler-Dixon',
-	'maydayheating@hotmail.com': 'Martin May',
 	'alfiefitz1987@outlook.com': 'Alfie Fitzpatrick',
-	'liam.barnes@yourheat.co.uk': 'Liam Barnes',
-	'george.downs@yourheat.co.uk': 'George Downs',
-	'carn.prior@yourheat.co.uk': 'Carn Prior'
+	'alan.brooks@yourheat.co.uk': 'Alan Brooks',
+	'nick.cook@yourheat.co.uk': 'Nick Cook'
 }
 
 engineer_postcode_dict = {
-	'Kevin Harvey (SM5)': 'kevin.harvey@yourheat.co.uk',
-	'Ben Pike (SS12)': 'pikeben28@yahoo.co.uk',
-	'Matthew Peters (ME8)': 'petersheatingservices@yahoo.com',
-	'Dave Easton (ME14)': 'dave_easton@me.com',
-	'Andy Douglas (ME12)': 'adouglas241@googlemail.com',
-	'Adam Simmons (TN10)': 'adam.simmons@yourheat.co.uk',
-	'Terry Martin (TN17)': 'terry.martin@yourheat.co.uk',
-	'Tom Sharp (EN9)': 'tom.sharp@yourheat.co.uk',
-	'Keith Myler-Dixon (CM2)': 'keith.myler-dixon@yourheat.co.uk',
-	'Martin May (KT)': 'maydayheating@hotmail.com',
 	'Alfie Fitzpatrick (TN25)': 'alfiefitz1987@outlook.com',
-	'Liam Barnes (ME12)' : 'liam.barnes@yourheat.co.uk',
-	'George Downs (SS7)' : 'george.downs@yourheat.co.uk',
-	'Carn Prior (TN24)' : 'carn.prior@yourheat.co.uk'
+	'Alan Brooks (ME6)': 'alan.brooks@yourheat.co.uk',
+	'Nick Cook (ME6)': 'nick.cook@yourheat.co.uk'
 }
 
 engineer_calendar_dict = {
-	'Kevin Harvey': 'kevin.harvey@yourheat.co.uk',
-	'Ben Pike': 'ben.pike@yourheat.co.uk',
-	'Matthew Peters': 'matthew.peters@yourheat.co.uk',
-	'Dave Easton': 'dave.easton@yourheat.co.uk',
-	'Andy Douglas': 'andy.douglas@yourheat.co.uk',
-	'Adam Simmons': 'adam.simmons@yourheat.co.uk',
-	'Terry Martin': 'terry.martin@yourheat.co.uk',
-	'Tom Sharp': 'tom.sharp@yourheat.co.uk',
-	'Keith Myler-Dixon': 'keith.myler-dixon@yourheat.co.uk',
-	'Martin May': 'martin.may@yourheat.co.uk',
 	'Alfie Fitzpatrick': 'alfie.fitzpatrick@yourheat.co.uk',
-	'Liam Barnes': 'liam.barnes@yourheat.co.uk',
-	'George Downs': 'george.downs@yourheat.co.uk',
-	'Carn Prior': 'carn.prior@yourheat.co.uk' 
+	'Alan Brooks': 'alan.brooks@yourheat.co.uk',
+	'Nick Cook': 'nick.cook@yourheat.co.uk'
 }
