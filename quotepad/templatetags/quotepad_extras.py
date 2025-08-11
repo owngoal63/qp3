07abe70f182f8ce7ff77c0dbd3954d8b831b5f09
Rorywork <Rorywork95@outlook.com>
2025-08-11 16:01:59 +0100

    APR change and new payment section on pdf form.

--- quotepad/templatetags/quotepad_extras.py
+++ quotepad/templatetags/quotepad_extras.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User, Group
+from decimal import Decimal, InvalidOperation
 from django import template
 
 register = template.Library()
@@ -7,3 +8,20 @@ register = template.Library()
 def has_group(user, group_name):
     group = Group.objects.get(name=group_name)
     return True if group in user.groups.all() else False
+
+
+@register.filter
+def subtract(value, arg):
+    """Subtract arg from value"""
+    try:
+        # Remove commas and convert to Decimal
+        val1 = Decimal(str(value).replace(',', ''))
+        val2 = Decimal(str(arg).replace(',', ''))
+        
+        result = val1 - val2
+        return result
+        
+    except Exception as e:
+        print(f"ERROR in subtract filter: {e}")
+        print(f"ERROR: value = {repr(value)}, arg = {repr(arg)}")
+        return 0
